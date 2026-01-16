273219347ac94686f11f2cf690024e673544af21
shijas <shijas@example.com>
2026-01-17 02:08:06 +0530

    Integrate Reown (WalletConnect) AppKit

---
+++ app/context/index.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { wagmiAdapter, projectId, networks } from '@/app/config';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createAppKit } from '@reown/appkit/react';
+import { base } from '@reown/appkit/networks';
+import React, { type ReactNode } from 'react';
+import { cookieToInitialState, WagmiProvider, type Config } from 'wagmi';
+
+const queryClient = new QueryClient();
+
+if (!projectId) {
+    throw new Error('Project ID is not defined');
+}
+
+const metadata = {
+    name: 'BADS',
+    description: 'Decentralized Advertising on Farcaster',
+    url: 'https://bads.app',
+    icons: ['https://assets.reown.com/reown-profile-pic.png'],
+};
+
+// Create the modal
+createAppKit({
+    adapters: [wagmiAdapter],
+    projectId,
+    networks,
+    defaultNetwork: base,
+    metadata: metadata,
+    features: {
+        analytics: true,
+    },
+    themeMode: 'dark',
+    themeVariables: {
+        '--w3m-accent': '#0052FF',
+        '--w3m-border-radius-master': '1px'
+    }
+});
+
+function ContextProvider({ children, cookies }: { children: ReactNode; cookies: string | null }) {
+    const initialState = cookieToInitialState(wagmiAdapter.wagmiConfig as Config, cookies);
+
+    return (
+        <WagmiProvider config={wagmiAdapter.wagmiConfig as Config} initialState={initialState}>
+            <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+        </WagmiProvider>
+    );
+}
+
+export default ContextProvider;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
 import "@farcaster/auth-kit/styles.css";
 import { Providers } from "./providers";
+import { headers } from "next/headers";
+import ContextProvider from "@/app/context";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const outfit = Outfit({ subsets: ["latin"], variable: "--font-outfit" });
@@ -13,17 +15,21 @@ export const metadata: Metadata = {
   viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookies = (await headers()).get('cookie');
+
   return (
     <html lang="en">
       <body className={`${inter.variable} ${outfit.variable}`}>
-        <Providers>
-          {children}
-        </Providers>
+        <ContextProvider cookies={cookies}>
+          <Providers>
+            {children}
+          </Providers>
+        </ContextProvider>
       </body>
     </html>
   );
