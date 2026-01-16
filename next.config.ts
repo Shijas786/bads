f9a3cb137b91914a7a2bb128eae628c13f1a0ca7
shijas <shijas@example.com>
2026-01-17 02:30:34 +0530

    Configure Next.js to ignore server-side wallet module errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['pino-pretty', 'lokijs', 'encoding', '@coinbase/cdp-sdk'],
+  webpack: (config) => {
+    config.externals.push({
+      'pino-pretty': 'commonjs pino-pretty',
+      'lokijs': 'commonjs lokijs',
+      'encoding': 'commonjs encoding',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
